17441de5c0b5ebbb9160d1aed152edacb3e5c3f0
Jade Beaujour <jadejuliette.beaujour@gmail.com>
2023-11-13 15:04:12 +0100

    lol

--- src/modele/Assurance.java
+++ src/modele/Assurance.java
@@ -1,5 +1,37 @@
 package modele;
-
 public class Assurance {
 
+	private String numéroPolice;
+	private int montantInit;
+	private Immeuble immeuble;
+	
+	public Assurance(String numéroPolice, int montantInit, Immeuble immeuble) {
+		this.numéroPolice = numéroPolice;
+		this.montantInit = montantInit;
+		this.immeuble = immeuble;
+	}
+
+	public String getNuméroPolice() {
+		return numéroPolice;
+	}
+
+	public void setNuméroPolice(String numéroPolice) {
+		this.numéroPolice = numéroPolice;
+	}
+
+	public int getMontantInit() {
+		return montantInit;
+	}
+
+	public void setMontantInit(int montantInit) {
+		this.montantInit = montantInit;
+	}
+
+	public Immeuble getImmeuble() {
+		return immeuble;
+	}
+
+	public void setImmeuble(Immeuble immeuble) {
+		this.immeuble = immeuble;
+	}
 }
