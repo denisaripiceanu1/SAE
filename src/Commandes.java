302962b7edb203ae2dd0dd92b395a8dfa6c72aa8
Nesrine <nesrineben.317@gmail.com>
2023-11-20 15:08:57 +0100

    gtg

---
+++ src/Commandes.java
@@ -0,0 +1,12 @@
+
+public class Commandes {
+
+	// pour envoyer ce que j'ai fait:
+	// - git add .
+	// - git commit -m "message nimporte"
+	// - git push
+
+	// pour recuperer les trucs des autres:
+	// - git pull origin main
+
+}
