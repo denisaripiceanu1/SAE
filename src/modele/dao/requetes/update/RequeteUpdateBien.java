4e956a9c4ff0c2a49622eec59b209a9596833a12
IliesBenmansour <131273105+IliesBenmansour@users.noreply.github.com>
2023-12-04 15:03:15 +0100

    message nimporte

--- src/modele/dao/requetes/update/RequeteUpdateBien.java
+++ src/modele/dao/requetes/update/RequeteUpdateBien.java
@@ -1,5 +1,30 @@
 package modele.dao.requetes.update;
 
-public class RequeteUpdateBien {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import modele.Bien;
+import modele.dao.requetes.Requete;
+
+public class RequeteUpdateBien implements Requete<Bien>{
+
+
+		@Override
+		public String requete() {
+			return "UPDATE Bien SET surface_habitable = ?, nb_pieces = ?, num_etages = ?, date_acquisition = ?, Id_Immeuble = ? WHERE Id_Bien = ?";
+		}
+		@Override
+		public void parametres(PreparedStatement prSt, Bien data) throws SQLException {
+			prSt.setDouble(1, data.getSurfaceHabitable());
+			prSt.setInt(2, data.getNbPieces());
+			prSt.setInt(3, data.getNumEtage());
+			prSt.setString(4, data.getDateAcquisition());
+			prSt.setString(5, data.getImmeuble().toString());
+			prSt.setString(6, data.getIdBien().toString()); // cle primaire 
+	}
+		@Override
+		public void parametres(PreparedStatement prSt, String... id) throws SQLException {
+			// TODO Auto-generated method stub
+			
+		}
 }
