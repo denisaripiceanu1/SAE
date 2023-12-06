4e6c20b20606cc8adf22c4e7063cc75b18e5761c
Jade Beaujour <jadejuliette.beaujour@gmail.com>
2023-12-06 12:04:09 +0100

    Insertion

--- src/vue/insertion/Fenetre_InsertionLogement.java
+++ src/vue/insertion/Fenetre_InsertionLogement.java
@@ -24,6 +24,8 @@ public class Fenetre_InsertionLogement extends JInternalFrame {
 	private JTextField textField_NumEtage;
 	private JButton btnAjouter;
 	private JButton btnAnnuler;
+	private JButton btnAjouterCompteur;
+	private JButton btnAjouterQuotite;
 
 	/**
 	 * Launch the application.
@@ -67,42 +69,42 @@ public class Fenetre_InsertionLogement extends JInternalFrame {
 		JComboBox comboBox_typeDeLogement = new JComboBox();
 		comboBox_typeDeLogement.setModel(new DefaultComboBoxModel(new String[] {"Appartement", "Maison", "Garage"}));
 		comboBox_typeDeLogement.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Type", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, null));
-		comboBox_typeDeLogement.setBounds(272, 142, 189, 39);
+		comboBox_typeDeLogement.setBounds(376, 119, 189, 39);
 		panel.add(comboBox_typeDeLogement);
 		
 		textField_IdLogement = new JTextField();
 		textField_IdLogement.setColumns(10);
 		textField_IdLogement.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Id Logement", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-		textField_IdLogement.setBounds(271, 92, 190, 40);
+		textField_IdLogement.setBounds(145, 118, 190, 40);
 		panel.add(textField_IdLogement);
 		
 		textField_SurfaceHabitable = new JTextField();
 		textField_SurfaceHabitable.setColumns(10);
 		textField_SurfaceHabitable.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Surface habitable", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-		textField_SurfaceHabitable.setBounds(271, 191, 190, 40);
+		textField_SurfaceHabitable.setBounds(145, 193, 190, 40);
 		panel.add(textField_SurfaceHabitable);
 		
 		textField_NbPièces = new JTextField();
 		textField_NbPièces.setColumns(10);
 		textField_NbPièces.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Nombre de pi\u00E8ces", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-		textField_NbPièces.setBounds(271, 241, 190, 40);
+		textField_NbPièces.setBounds(375, 193, 190, 40);
 		panel.add(textField_NbPièces);
 		
 		textField_DateAcquisition = new JTextField();
 		textField_DateAcquisition.setColumns(10);
 		textField_DateAcquisition.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Date d'acquisition", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-		textField_DateAcquisition.setBounds(271, 291, 190, 40);
+		textField_DateAcquisition.setBounds(145, 269, 190, 40);
 		panel.add(textField_DateAcquisition);
 		
 		textField_NumEtage = new JTextField();
 		textField_NumEtage.setColumns(10);
 		textField_NumEtage.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Num\u00E9ro d'\u00E9tage", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-		textField_NumEtage.setBounds(271, 341, 190, 40);
+		textField_NumEtage.setBounds(375, 269, 190, 40);
 		panel.add(textField_NumEtage);
 		
 		btnAjouter = new JButton("Ajouter");
 		btnAjouter.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnAjouter.setBounds(241, 445, 94, 31);
+		btnAjouter.setBounds(235, 445, 94, 31);
 		btnAjouter.setForeground(Color.WHITE);
 		btnAjouter.setBackground(new Color(0, 102, 204));
 		panel.add(btnAjouter);
@@ -113,6 +115,14 @@ public class Fenetre_InsertionLogement extends JInternalFrame {
 		btnAnnuler.setForeground(Color.WHITE);
 		btnAnnuler.setBackground(new Color(0, 102, 204));
 		panel.add(btnAnnuler);
+		
+		btnAjouterCompteur = new JButton("Ajouter un compteur");
+		btnAjouterCompteur.setBounds(291, 390, 154, 23);
+		panel.add(btnAjouterCompteur);
+		
+		btnAjouterQuotite = new JButton("Ajouter une quotité");
+		btnAjouterQuotite.setBounds(290, 356, 154, 23);
+		panel.add(btnAjouterQuotite);
 
 	}
 }
