179dc6c68bfb9c2513beb771e7ceb16950f5c527
Jade Beaujour <jadejuliette.beaujour@gmail.com>
2023-12-11 10:28:37 +0100

    trucs

--- src/controleur/insertion/GestionInsertionTravaux.java
+++ src/controleur/insertion/GestionInsertionTravaux.java
@@ -1,5 +1,32 @@
 package controleur.insertion;
 
-public class GestionInsertionTravaux {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+
+import vue.Fenetre_Accueil;
+import vue.insertion.Fenetre_InsertionTravaux;
+
+public class GestionInsertionTravaux implements ActionListener{
+	
+	private Fenetre_InsertionTravaux fit;
+	
+	public GestionInsertionTravaux(Fenetre_InsertionTravaux fit) {
+		this.fit = fit;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton btn = (JButton) e.getSource();
+		Fenetre_Accueil fenetre_Principale = (Fenetre_Accueil) this.fit.getTopLevelAncestor(); //fenetre dans laquelle on ouvre des internal frame
+		switch (btn.getText()) {
+		case "Ajouter":
+			
+			break;
+		case "Annuler":
+			this.fit.dispose();
+			break;
+		}
+	}
 }
