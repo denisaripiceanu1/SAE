41f3fef2928ec8c68414691d2c6ff6e6e2b9f7d5
Jade Beaujour <jadejuliette.beaujour@gmail.com>
2023-12-11 12:32:48 +0100

    reste des trucs

---
+++ src/vue/insertion/Fenetre_Locataire_SoldeToutCompte.java
@@ -0,0 +1,14 @@
+package vue.insertion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class Fenetre_Locataire_SoldeToutCompte extends JInternalFrame {
+
+	public Fenetre_Locataire_SoldeToutCompte() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
