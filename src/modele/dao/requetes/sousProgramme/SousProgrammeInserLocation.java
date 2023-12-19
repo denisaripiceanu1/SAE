39e59238487df8f169d179580cdccedee068fda1
denisaripiceanu1 <denisa.ripiceanu@etu.iut-tlse3.fr>
2023-12-19 19:15:49 +0100

    Modif

--- src/modele/dao/requetes/sousProgramme/SousProgrammeInserLocation.java
+++ src/modele/dao/requetes/sousProgramme/SousProgrammeInserLocation.java
@@ -20,23 +20,41 @@ public class SousProgrammeInserLocation implements SousProgramme<Louer> {
 
 	@Override
 	public void parametres(PreparedStatement prSt, Louer donnee) throws SQLException {
-		prSt.setString(1, donnee.getLocataire().getIdLocataire());
-		prSt.setString(2, donnee.getBien().getIdBien());
-		prSt.setDate(3, java.sql.Date.valueOf (donnee.getDateDebut()));
-		
-		    // Paramètre potentiellement nul
-		    if (donnee.getNbMois() == null) {
-		        prSt.setNull(4, java.sql.Types.INTEGER);
-		    } else {
-		        prSt.setInt(4, donnee.getNbMois());
-		    }
+	    prSt.setString(1, donnee.getLocataire().getIdLocataire());
+	    prSt.setString(2, donnee.getBien().getIdBien());
+	    prSt.setDate(3, java.sql.Date.valueOf(donnee.getDateDebut()));
 
-		    prSt.setDouble(5, donnee.getLoyerTTC());
-		    prSt.setDouble(6, donnee.getProvision_chargeMens_TTC());
-		    prSt.setDouble(7, donnee.getCautionTTC());
-		    prSt.setString(8, donnee.getBail());
-		    prSt.setString(9, donnee.getEtat_lieux());
-		}
+	    // Paramètre potentiellement nul
+	    if (donnee.getNbMois() == 0) {
+	        prSt.setNull(4, java.sql.Types.INTEGER);
+	    } else {
+	        prSt.setInt(4, donnee.getNbMois());
+	    }
+
+	    prSt.setDouble(5, donnee.getLoyerTTC());
+	    prSt.setDouble(6, donnee.getProvision_chargeMens_TTC());
+	    prSt.setDouble(7, donnee.getCautionTTC());
+	    prSt.setString(8, donnee.getBail());
+	    prSt.setString(9, donnee.getEtat_lieux());
+
+	    // Paramètres potentiellement nuls
+	    if (donnee.getDateDepart() == null) {
+	        prSt.setNull(10, java.sql.Types.DATE);
+	    } else {
+	        prSt.setDate(10, java.sql.Date.valueOf(donnee.getDateDepart()));
+	    }
+
+	    prSt.setInt(11, donnee.getLoyerPaye());
+
+	    if (donnee.getIcc() == null) {
+	        prSt.setNull(12, java.sql.Types.DOUBLE);
+	    } else {
+	        // Remplacez ce qui suit par le véritable code pour obtenir la valeur de l'objet ICC
+	        prSt.setDouble(12, donnee.getIcc().getIndice());
+	    }
+
+	    prSt.setDouble(13, donnee.getMontantReelPaye());
+	}
 
 
 }
