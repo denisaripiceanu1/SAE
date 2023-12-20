fa04eb584432d6e11304472de6f35143d2c015fc
denisaripiceanu1 <denisa.ripiceanu@etu.iut-tlse3.fr>
2023-12-20 04:30:30 +0100

    Modif

--- src/modele/dao/requetes/sousProgramme/SousProgrammeInserLocation.java
+++ src/modele/dao/requetes/sousProgramme/SousProgrammeInserLocation.java
@@ -20,37 +20,34 @@ public class SousProgrammeInserLocation implements SousProgramme<Louer> {
 
 	@Override
 	public void parametres(PreparedStatement prSt, Louer donnee) throws SQLException {
-	    prSt.setString(1, donnee.getLocataire().getIdLocataire());
-	    prSt.setString(2, donnee.getBien().getIdBien());
-	    prSt.setDate(3, java.sql.Date.valueOf(donnee.getDateDebut()));
+		prSt.setString(1, donnee.getLocataire().getIdLocataire());
+		prSt.setString(2, donnee.getBien().getIdBien());
+		prSt.setDate(3, java.sql.Date.valueOf(donnee.getDateDebut()));
 
-	    // Paramètre potentiellement nul
+		// Paramètre potentiellement nul
 
-	        prSt.setNull(4, java.sql.Types.INTEGER);
-	
-	    prSt.setDouble(5, donnee.getLoyerTTC());
-	    prSt.setDouble(6, donnee.getProvision_chargeMens_TTC());
-	    prSt.setDouble(7, donnee.getCautionTTC());
-	    prSt.setString(8, donnee.getBail());
-	    prSt.setString(9, donnee.getEtat_lieux());
+		prSt.setNull(4, java.sql.Types.INTEGER);
 
-	    // Paramètres potentiellement nuls
-	    if (donnee.getDateDepart() == null) {
-	        prSt.setNull(10, java.sql.Types.DATE);
-	    } else {
-	        prSt.setDate(10, java.sql.Date.valueOf(donnee.getDateDepart()));
-	    }
+		prSt.setDouble(5, donnee.getLoyerTTC());
+		prSt.setDouble(6, donnee.getProvision_chargeMens_TTC());
+		prSt.setDouble(7, donnee.getCautionTTC());
+		prSt.setString(8, donnee.getBail());
+		prSt.setString(9, donnee.getEtat_lieux());
 
-	    prSt.setInt(11, donnee.getLoyerPaye());
+		// Paramètres potentiellement nuls
+		if (donnee.getDateDepart() == null) {
+			prSt.setNull(10, java.sql.Types.DATE);
+		} else {
+			prSt.setDate(10, java.sql.Date.valueOf(donnee.getDateDepart()));
+		}
 
-	   
-	        prSt.setString(12, "2023");
-	    
-	        prSt.setString(13, "1");
+		prSt.setInt(11, donnee.getLoyerPaye());
 
-	    prSt.setDouble(14, donnee.getMontantReelPaye());
+		prSt.setString(12, "2023"); // annee
+
+		prSt.setString(13, "1"); // trimestre
+
+		prSt.setDouble(14, donnee.getMontantReelPaye());
 	}
 
-
 }
-
