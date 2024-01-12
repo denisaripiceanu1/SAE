701076270227477682a786ea56a82b371bfff57b
jademlf <jademalefoy1605@gmail.com>
2024-01-12 17:30:50 +0100

    info loca

--- src/vue/insertion/Fenetre_AffichageInfoLocataire.java
+++ src/vue/insertion/Fenetre_AffichageInfoLocataire.java
@@ -4,12 +4,15 @@ import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.border.LineBorder;
 
 import controleur.insertion.GestionAffichageInfoLocataire;
@@ -25,6 +28,8 @@ public class Fenetre_AffichageInfoLocataire extends JInternalFrame {
     private JButton btnAnnuler;
     private GestionModificationLocataire gestionModificationLocataire;
     private GestionAffichageInfoLocataire gestionClic;
+	private JScrollPane scrollPane_locataireSoldeToutCompte;
+	private JTable table_soldeToutCompte;
 
     public Fenetre_AffichageInfoLocataire() {
         // Initialisation du gestionnaire d'affichage et de modification du locataire
@@ -60,7 +65,7 @@ public class Fenetre_AffichageInfoLocataire extends JInternalFrame {
 
         // Bouton Régularisation des charges
         JButton btnRegularisationCharges = new JButton("Régularisation des charges");
-        btnRegularisationCharges.setBounds(490, 130, 200, 25);
+        btnRegularisationCharges.setBounds(436, 144, 200, 25);
         panel.add(btnRegularisationCharges);
         btnRegularisationCharges.addActionListener(gestionClic);
 
@@ -84,28 +89,28 @@ public class Fenetre_AffichageInfoLocataire extends JInternalFrame {
         textField_Prenom.setColumns(10);
         textField_Prenom.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Prénom ",
                 TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-        textField_Prenom.setBounds(248, 182, 190, 40);
+        textField_Prenom.setBounds(33, 233, 190, 40);
         panel.add(textField_Prenom);
 
         textField_Telephone = new JTextField();
         textField_Telephone.setColumns(10);
         textField_Telephone.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)),
                 "n° Téléphone", TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-        textField_Telephone.setBounds(33, 248, 190, 40);
+        textField_Telephone.setBounds(33, 286, 190, 40);
         panel.add(textField_Telephone);
 
         textField_Mail = new JTextField();
         textField_Mail.setColumns(10);
         textField_Mail.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Mail", TitledBorder.LEADING,
                 TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-        textField_Mail.setBounds(33, 300, 190, 40);
+        textField_Mail.setBounds(33, 337, 190, 40);
         panel.add(textField_Mail);
 
         textField_DateN = new JTextField();
         textField_DateN.setColumns(10);
         textField_DateN.setBorder(new TitledBorder(new LineBorder(new Color(0, 102, 204)), "Date de naissance",
                 TitledBorder.LEADING, TitledBorder.ABOVE_TOP, null, new Color(0, 0, 0)));
-        textField_DateN.setBounds(33, 364, 190, 40);
+        textField_DateN.setBounds(33, 388, 190, 40);
         panel.add(textField_DateN);
 
         // Bouton Retour
@@ -124,25 +129,47 @@ public class Fenetre_AffichageInfoLocataire extends JInternalFrame {
         JSeparator separator = new JSeparator();
         separator.setOrientation(SwingConstants.VERTICAL);
         separator.setBackground(new Color(0, 102, 204));
-        separator.setBounds(450, 84, 20, 344);
+        separator.setBounds(313, 93, 20, 344);
         panel.add(separator);
 
         JLabel lblOprations = new JLabel("Opérations");
         lblOprations.setFont(new Font("Tahoma", Font.PLAIN, 14));
-        lblOprations.setBounds(480, 88, 137, 25);
+        lblOprations.setBounds(358, 88, 137, 25);
         panel.add(lblOprations);
 
         // Bouton Solde tout compte
         JButton btnSoldeToutCompte = new JButton("Solde tout compte");
-        btnSoldeToutCompte.setBounds(500, 198, 200, 25);
+        btnSoldeToutCompte.setBounds(426, 223, 210, 25);
         panel.add(btnSoldeToutCompte);
         btnSoldeToutCompte.addActionListener(gestionClic);
 
         JLabel lblDpartLoca = new JLabel("Départ de votre locataire :");
         lblDpartLoca.setFont(new Font("Tahoma", Font.PLAIN, 11));
-        lblDpartLoca.setBounds(532, 167, 137, 25);
+        lblDpartLoca.setBounds(451, 196, 137, 25);
         panel.add(lblDpartLoca);
+        
+        
+        this.scrollPane_locataireSoldeToutCompte = new JScrollPane();
+		this.scrollPane_locataireSoldeToutCompte.setBorder(new LineBorder(new Color(0, 102, 204), 1, true));
+		this.scrollPane_locataireSoldeToutCompte.setBounds(343, 259, 380, 107);
+		panel.add(this.scrollPane_locataireSoldeToutCompte);
+
+		// Table pour afficher les données d'entreprise
+		this.table_soldeToutCompte = new JTable();
+		this.table_soldeToutCompte
+				.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"Provisions sur charges","Charges réelles payées", "Caution", "Travaux imputables", "Reste"
+			}
+		));
+		scrollPane_locataireSoldeToutCompte.setViewportView(this.table_soldeToutCompte);
+
+        
     }
+    
 
     // Getters pour récupérer les valeurs des champs
     public JTextField getTextField_Nom() {
