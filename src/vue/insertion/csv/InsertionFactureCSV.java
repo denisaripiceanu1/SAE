0bb3690d9835e911b4e8010bc0e19e210a47ed90
IliesBenmansour <iliesmarouane@gmail.com>
2024-01-16 18:55:51 +0100

    regle le soucis

--- src/vue/insertion/csv/InsertionFactureCSV.java
+++ src/vue/insertion/csv/InsertionFactureCSV.java
@@ -2,8 +2,6 @@ package vue.insertion.csv;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
@@ -24,15 +22,7 @@ public class InsertionFactureCSV extends JInternalFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				chemin.choisirChemin();
-				try {
-					lire.lireCSV(chemin.getSelectedFilePath());
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				lire.lireCSV(chemin.getSelectedFilePath()); // ma bite je le fais lui aussi
 			}
 		});
 		btnNewButton.setBounds(235, 176, 252, 97);
