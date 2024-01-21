4ab3218294e89e950aa0a98d871fea7186c3c864
denisaripiceanu1 <denisa.ripiceanu@etu.iut-tlse3.fr>
2024-01-21 11:03:46 +0100

    sql

--- src/vue/insertion/Fenetre_InsertionBien.java
+++ src/vue/insertion/Fenetre_InsertionBien.java
@@ -1,16 +1,15 @@
 package vue.insertion;
 
 import javax.swing.JButton;
+
 import javax.swing.JComboBox;
 import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.Color;
-import java.awt.Font;
 
 import controleur.insertion.GestionInsertionBien;
 
